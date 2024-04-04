62cffba4a59ba930c66979cdf721151413d8af36
Taylor Seager <taylorseager718@gmail.com>
2024-04-03 19:49:02 -0500

    counter styling and buttons updated

--- components/Counter.js
+++ components/Counter.js
@@ -23,11 +23,11 @@ export default function Counter({ title }) {
 
   return (
     <>
-      <h1>{title}</h1>
-      <h2>{value}</h2>
-      <button type="button" onClick={() => handleClick('Increment')}>Increment</button>
-      <button type="button" onClick={() => handleClick('Decrement')}>Decrement</button>
-      <button type="button" onClick={() => handleClick('Rest')}>Reset</button>
+      <h1 style={{ color: 'white' }}>{title}</h1>
+      <h2 style={{ color: 'white' }}>{value}</h2>
+      <button className="btn btn-success" type="button" onClick={() => handleClick('Increment')}>Increment</button>
+      {value <= 0 ? '' : <button className="btn btn-danger" type="button" onClick={() => handleClick('Decrement')}>Decrement</button>}
+      <button className="btn btn-light" type="button" onClick={() => handleClick('Reset')}>Reset</button>
     </>
   );
 }
